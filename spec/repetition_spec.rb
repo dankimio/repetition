b3429dd301da2b56501e37e57bb8bf1a0ad3fb30
Scott Harvey <scott@scottharvey.co>
2017-04-11 11:30:13 +0700

    Added comments and cleared out some unused code

--- spec/repetition_spec.rb
+++ spec/repetition_spec.rb
@@ -3,12 +3,11 @@ require 'spec_helper'
 describe Repetition do
   before :each do
     class Test
-      attr_accessor :easiness_factor, :number_repetitions, :quality_of_last_recall, :next_repetition, :repetition_interval, :last_studied
+      attr_accessor :easiness_factor, :number_repetitions, :next_repetition, :repetition_interval, :last_studied
 
       include Repetition
     end
     @card = Test.new
-    @card.reset_spaced_repetition_data
   end
 
   it 'has a version number' do
@@ -16,16 +15,7 @@ describe Repetition do
   end
 
   it 'works when included' do
-    expect(@card).to respond_to :easiness_factor, :number_repetitions, :quality_of_last_recall, :next_repetition, :repetition_interval, :last_studied
-  end
-
-  it 'initializes values properly' do
-    expect(@card.easiness_factor).to eq(2.5)
-    expect(@card.number_repetitions).to eq(0)
-    expect(@card.quality_of_last_recall).to be_nil
-    expect(@card.next_repetition).to be_nil
-    expect(@card.repetition_interval).to eq(0)
-    expect(@card.last_studied).to be_nil
+    expect(@card).to respond_to :easiness_factor, :number_repetitions, :next_repetition, :repetition_interval, :last_studied
   end
 
   it 'schedules for tommorow when interval = 0 and quality = 4' do
@@ -77,5 +67,12 @@ describe Repetition do
 
     @card.process_recall_result(4)
     expect(@card.next_repetition).to eq(Date.today + 1)
+  end
+
+  it 'allows for a recall date to be manually set' do
+    date = Date.today - 21
+    @card.process_recall_result(0, date)
+    expect(@card.last_studied).to eq(date)
+    expect(@card.next_repetition).to eq(date)
   end
 end
