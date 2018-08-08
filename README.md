8518bebf59e6572d2b1ff1027b225f2ebe2e112b
Dan Kim <itsdanya@gmail.com>
2018-08-08 14:50:08 +0300

    Refactor module into class

--- README.md
+++ README.md
@@ -1,68 +1,33 @@
 # Repetition gem
+
 Spaced repetition module which can be used as a mixin in Ruby apps. SuperMemo 2 is used as a repetition algorithm.
 
 ### Installation
+
 Add to your Gemfile
 
-    gem 'repetition'
+```ruby
+gem 'repetition'
+```
 
 or install from RubyGems
 
-    gem install repetition
-
-
-### Rails
-
-Create a migration to add the new fields to your model.
-```ruby
-change_table :cards do |t|
-  t.decimal :easiness_factor, precision: 2, scale: 1, default: 2.5, null: false
-  t.integer :number_repetitions, default: 0, null: false
-  t.integer :quality_of_last_recall
-  t.date :next_repetition
-  t.integer :repetition_interval
-  t.date :last_studied
-end
-```
+    $ gem install repetition
 
 ### Usage
-Include module in your class or ActiveRecord model
+
 ```ruby
-class Card < ActiveRecord::Base
-  # some code
-  include Repetition
-end
+# Default values are provided
+repetition = Repetition.new(easiness_factor: 2.5, interval: 0, repetitions: 0)
+# Quality of recall: 0..5
+# Returns: date of the next recall => <Date: 2018-08-09>
+repetition.recall(4)
 ```
-
-This will add a bunch of properties to your model:
-* easyness_factor
-* number_repetitions
-* quality_of_last_recall
-* next_repetition
-* repetition_interval
-* last_studied
-
-Make sure that appropriate fields are created in your database using migrations
-
-You can now use all methods that are provided by gem, use `process_recall_result(quality)` method to update next repetition date. Method takes one argument which should be integer in range from 0 to 5 (0 - again, 5 - perfect).
-```ruby
-card = Card.first
-card.next_repetition # => nil
-card.process_recall_result(4)
-
-card.repetition_interval # => 1
-card.next_repetition # Tomorrow
-card.save # Don't forget to save your card!
-```
-
-### Changelog
-#### 1.0
-* Initial version
 
 ### Running Specs
 
 `bundle exec rake`
 
 ### Contributing
-Contributions are welcome! Feel free to post issues and create pull requests.
 
+Contributions are welcome! Feel free to post issues and create pull requests.
--- lib/repetition.rb
+++ lib/repetition.rb
@@ -1,6 +1,6 @@
 #
 # The Repetition module uses the Super Memo method to determine when to next
-# review an item. The quality_of_recall values are as follows:
+# review an item. The quality values are as follows:
 #
 # 5 - perfect response
 # 4 - correct response after a hesitation
@@ -11,55 +11,58 @@
 #
 # Find out more here: https://www.supermemo.com/english/ol/sm2.htm
 #
-require 'repetition/version'
+
 require 'date'
-module Repetition
-  def process_recall_result(quality_of_recall, date_of_recall = Date.today)
-    unless (0..5).include?(quality_of_recall)
+
+class Repetition
+  def initialize(interval: 0, easiness_factor: 2.5, repetitions: 0)
+    @easiness_factor = easiness_factor
+    @interval = interval
+    @repetitions = repetitions
+  end
+
+  def recall(quality)
+    unless (0..5).include?(quality)
       raise 'Invalid quality of recall. Should be in range from 0 to 5.'
     end
 
-    # Initialize values if they aren't set
-    self.easiness_factor ||= 2.5
-    self.number_repetitions ||= 0
-    self.repetition_interval ||= 0
-
-    if quality_of_recall < 3
+    if quality < 3
       # An incorrect recall is reset back to the beginning
-      self.number_repetitions = 0
-      self.repetition_interval = 0
-
-    elsif quality_of_recall == 3
+      @repetitions = 0
+      @interval = 0
+    elsif quality == 3
       # The item was correctly recalled but should be tested again quickly
-      self.repetition_interval = 0
-
+      @interval = 0
     else
       # The item was correctly recalled and we can review later on
-      self.number_repetitions += 1
+      @repetitions += 1
 
-      case number_repetitions
+      case @repetitions
       when 1
-        self.repetition_interval = 1
+        @interval = 1
       when 2
-        self.repetition_interval = 6
+        @interval = 6
       else
-        self.easiness_factor = calculate_easiness_factor(easiness_factor, quality_of_recall)
-        self.repetition_interval = repetition_interval * easiness_factor
+        @easiness_factor = calculate_easiness_factor(@easiness_factor, quality)
+        @interval = interval * easiness_factor
       end
     end
 
-    self.next_repetition = date_of_recall + repetition_interval
-    self.last_studied = date_of_recall
+    due_on
   end
 
-  def scheduled_to_recall?
-    !next_repetition.nil? && next_repetition <= Date.today
+  def due_on
+    today + @interval
   end
 
   private
 
-  def calculate_easiness_factor(easiness_factor, quality_of_recall)
-    result = easiness_factor - 0.8 + (0.28 * quality_of_recall) - (0.02 * quality_of_recall * quality_of_recall)
+  def today
+    @today ||= Date.today
+  end
+
+  def calculate_easiness_factor(easiness_factor, quality)
+    result = easiness_factor - 0.8 + (0.28 * quality) - (0.02 * quality * quality)
     result < 1.3 ? 1.3 : result
   end
 end
